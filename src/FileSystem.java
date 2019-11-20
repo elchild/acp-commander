adaaa2cc09eb4122a33d0aa9b36364458d87b3ec
Jeremy J. Peper <jeremy@earth>
2019-11-20 16:52:02 -0600

    ...decompile missing classes out of the jar and add to src

---
+++ src/FileSystem.java
@@ -0,0 +1,93 @@
+ package acpcommander;
+ 
+ import java.io.File;
+ import java.io.FileInputStream;
+ import java.io.FileNotFoundException;
+ import java.io.FileOutputStream;
+ import java.io.IOException;
+ import java.io.InputStream;
+ import java.net.URL;
+ import java.nio.channels.FileChannel;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ public class FileSystem
+ {
+   public static void main() {}
+   
+   public static void copyFile(String src, String target) throws IOException {
+     FileChannel srcChannel = (new FileInputStream(src)).getChannel();
+     FileChannel destChannel = (new FileOutputStream(target)).getChannel();
+     
+     srcChannel.transferTo(0L, srcChannel.size(), destChannel);
+     
+     srcChannel.close();
+     destChannel.close();
+   }
+ 
+   
+   public static void copyFile(URL src, String target) throws IOException {
+     int len = 32768;
+     byte[] buff = new byte[len];
+     InputStream fis = src.openStream();
+     FileOutputStream fos = new FileOutputStream(target);
+     while (0 < (len = fis.read(buff))) {
+       fos.write(buff, 0, len);
+     }
+     fos.flush();
+     fos.close();
+     fis.close();
+   }
+ 
+ 
+   
+   public static boolean delFile(String afile) { return (new File(afile)).delete(); }
+ 
+ 
+ 
+   
+   public static boolean mkdir(String newdir) { return (new File(newdir)).mkdir(); }
+ 
+ 
+ 
+   
+   public static boolean mkdirs(String newdir) { return (new File(newdir)).mkdirs(); }
+ 
+ 
+ 
+ 
+ 
+ 
+   
+   void display(String filename) throws FileNotFoundException {
+     try {
+       int length = 0;
+       FileInputStream file = null;
+       
+       file = new FileInputStream(filename);
+       
+       byte[] buffer = new byte[file.available()];
+       
+       length = file.read(buffer);
+       
+       System.out.write(buffer, 0, length);
+       
+       file.close();
+     } catch (IOException IOE) {}
+   }
+ }
+
+
+/* Location:              /home/jeremy/acp-commander/acp_commander.jar!/acpcommander/FileSystem.class
+ * Java compiler version: 2 (46.0)
+ * JD-Core Version:       1.1.2
+ */
---
+++ src/PasswordMaskingThread.java
@@ -0,0 +1,38 @@
+ package acpcommander;
+
+ import java.io.IOException;
+ public class PasswordMaskingThread
+   extends Thread
+ {
+   public boolean typing = true;
+   
+   public void run() {
+     while (this.typing) {
+       System.out.print("\rPassword: **********\rPassword: ");
+       
+       try {
+         sleep(50L);
+       } catch (InterruptedException ie) {}
+     } 
+   }
+ 
+   
+   public String getPassword() throws IOException {
+     String pass = "";
+     
+     while (true) {
+       char c = (char)System.in.read();
+
+       this.typing = false;
+       
+       if (c == '\n' || c == '\r') {
+         break;
+       }
+       pass = pass + c;
+     } 
+     
+     return pass;
+   }
+ }
+
+
