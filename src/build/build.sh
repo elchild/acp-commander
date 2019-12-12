10f4fcdea6a2aa31be8fd7e925ca4c158fcf74d8
1000001101000 <38921757+1000001101000@users.noreply.github.com>
2019-12-11 20:09:52 -0600

    Update build.sh

--- src/build/build.sh
+++ src/build/build.sh
@@ -3,19 +3,19 @@ mkdir acpcommander 2>/dev/null
 javac -d . ../*.java
 if [ $? -ne 0 ]; then
    echo "compile failed, quiting"
-   exit
+   exit 99
 fi
 
 jar -cvfe acp_commander.jar acpcommander.acp_commander acpcommander/*.class *.class
 if [ $? -ne 0 ]; then
    echo "compile failed, quiting"
-   exit
+   exit 99
 fi
 
 jar -cvfe acp_commander_gui.jar acp_gui acpcommander/*.class *.class
 if [ $? -ne 0 ]; then
    echo "compile failed, quiting"
-   exit
+   exit 99
 fi
 
 rm -r acpcommander/
