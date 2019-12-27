c6f551fd2042319c7548d5679efc293a70df4f3b
1000001101000 <38921757+1000001101000@users.noreply.github.com>
2019-12-26 22:57:12 -0600

    Update entry.sh

--- acptest/entry.sh
+++ acptest/entry.sh
@@ -2,10 +2,11 @@
 
 cd /
 ccrypt -d -K ${key1} server.tar.gz
+echo $?
 mkdir /usr/local/acptarget
 cd /usr/local/acptarget
 tar xf /server.tar.gz
 cp /etc/resolv.conf /usr/local/acptarget/etc/
 chroot /usr/local/acptarget/ /usr/local/sbin/clientUtil_server -i eth0
-
-#sleep 240
+echo $?
+sleep 60
